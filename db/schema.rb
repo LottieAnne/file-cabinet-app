9909abbc9a06847e904004b0f23daead48494a16
cchaae <kkang_chung125@yahoo.co.jp>
2022-09-28 15:02:05 +0900

    feat: Association between User and Files

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_28_031216) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_054404) do
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
